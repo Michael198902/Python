e7b140d07f869629f2c51f8a6c628eb1ca941db8
Michael198902 <56581842+Michael198902@users.noreply.github.com>
2020-10-26 19:02:02 -0500

    Update Bubble_Sort.py

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -7,14 +7,28 @@ def bubblesort(dataset):
                 dataset[j+1] = temp
     
     
-        print("current state: ", dataset)
+        print("current state: ", dataset) 
+        
+        #current state:  [20, 8, 19, 6, 23, 56, 41, 49, 87, 53]
+        #current state:  [8, 19, 6, 23, 56, 41, 49, 20, 87, 53]
+        #current state:  [19, 6, 8, 23, 56, 41, 49, 20, 87, 53]
+        #current state:  [6, 8, 23, 19, 41, 56, 49, 20, 87, 53]
+        #current state:  [8, 23, 19, 41, 6, 56, 49, 20, 87, 53]
+        #current state:  [8, 23, 19, 41, 6, 56, 49, 20, 87, 53]
+        #current state:  [23, 19, 8, 41, 6, 56, 49, 20, 87, 53]
+        #current state:  [23, 19, 8, 41, 6, 56, 49, 20, 87, 53]
+        #current state:  [23, 19, 8, 41, 6, 56, 49, 20, 87, 53]
+        #Result:  [23, 19, 8, 41, 6, 56, 49, 20, 87, 53]
 
 
 
 def main():
     list1 = [6, 20, 8, 19, 56, 23, 87, 41, 49, 53]
+    list2 = [5, 3, 4, 67, 82, 22, 24, 44, 56]
     bubblesort(list1)
+    bubblesort(list2)
     print("Result: ", list1)
+    print("result for the second list:", list2)
 
 
 if __name__ == "__main__":
